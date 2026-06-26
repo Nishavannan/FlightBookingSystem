ccdc57665ff155275b9c41cd2b4eeb4bfc813a9b
nisha <nishavannan@gmail.com>
2026-06-26 22:17:35 +0530

    Pushing flight, booking, Passenger object.

--- src/main/java/Booking.java
+++ src/main/java/Booking.java
@@ -1,2 +1,68 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Booking {
+    private final String id;
+    private final String fightId;
+    private final String passengerId;
+    private final int seatsBooked;
+    private final double amount;
+    private final LocalDateTime bookingTime;
+    private BookingStatus status;
+    private static final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public Booking(String id, String flightId, String passengerId, int seatsBooked, double amount ,LocalDateTime bookingTime, BookingStatus status){
+        this.id = id;
+        this.fightId = flightId;
+        this.passengerId = passengerId;
+        this.seatsBooked = seatsBooked;
+        this.amount = amount;
+        this.bookingTime = bookingTime;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFightId() {
+        return fightId;
+    }
+
+    public String getPassengerId() {
+        return passengerId;
+    }
+
+    public int getSeatsBooked() {
+        return seatsBooked;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public LocalDateTime getBookingTime() {
+        return bookingTime;
+    }
+
+    public BookingStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(BookingStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "id='" + id + '\'' +
+                ", fightId='" + fightId + '\'' +
+                ", passengerId='" + passengerId + '\'' +
+                ", seatsBooked=" + seatsBooked +
+                ", amount=" + amount +
+                ", bookingTime=" + bookingTime.format(fmt) +
+                ", status=" + status +
+                '}';
+    }
 }
--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,2 +1,92 @@
+import javax.print.DocFlavor;
+import javax.swing.text.DateFormatter;
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
 public class Flight {
+    private final String id;
+    private  String source;
+    private  String destination;
+    private  LocalDateTime departureTime;
+    private  LocalDateTime arrivalTime;
+    private int totalSeats;
+    private int availableSeats;
+    private double price;
+    private static final DateTimeFormatter FMT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+    public Flight(String id, String source, String destination, LocalDateTime departureTime, LocalDateTime arrivalTime, int totalSeats, double price){
+        this.id = id;
+        this.source = source;
+        this.destination = destination;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.totalSeats = totalSeats;
+        this.availableSeats = totalSeats;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public LocalDateTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public LocalDateTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public int getTotalSeats() {
+        return totalSeats;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
+
+    @Override
+    public String toString() {
+        return "Flight{" +
+                "id='" + id + '\'' +
+                ", source='" + source + '\'' +
+                ", destination='" + destination + '\'' +
+                ", departureTime=" + departureTime.format(FMT) +
+                ", arrivalTime=" + arrivalTime.format(FMT) +
+                ", totalSeats=" + totalSeats +
+                ", availableSeats=" + availableSeats +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(!(o instanceof Flight)) return false;
+        Flight that = (Flight) o;
+        return this.id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
