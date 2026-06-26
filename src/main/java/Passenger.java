871393659b425ae0a3059d29a802e0a2f412a337
nisha <nishavannan@gmail.com>
2026-06-26 19:19:22 +0530

    Pushing flight, booking, Passenger object.

---
+++ src/main/java/Passenger.java
@@ -0,0 +1,52 @@
+import java.util.Objects;
+
+public class Passenger {
+    private final String id;
+    private String name;
+    private String phno;
+    private String email;
+
+    public Passenger(String id,String name, String phno, String email){
+        this.id = id;
+        this.name = name;
+        this.phno = phno;
+        this.email = email;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhno() {
+        return phno;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Passenger{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", phno='" + phno + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public boolean equals(Object o){
+        if(this == o) return true;
+        if(!(o instanceof Passenger))  return false;
+        Passenger that = (Passenger) o;
+        return this.id.equals(that.id);
+    }
+
+    public int hashCode(){
+        return Objects.hash(id);
+    }
+}
